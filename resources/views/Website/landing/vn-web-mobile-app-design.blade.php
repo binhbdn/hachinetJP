8143079d1e235a7bcc83957b71adf071b9595eb7
binhbdn <binh.buidn@gmail.com>
2020-11-06 17:34:31 +0700

    Add new file: new langding page - Thiết kế Website

---
+++ resources/views/Website/landing/vn-web-mobile-app-design.blade.php
@@ -0,0 +1,327 @@
+<!doctype html>
+<html class="no-js" lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="x-ua-compatible" content="ie=edge">
+    <title>Thiết kế Website - Mobile App</title>
+    <meta name="description" content="Hachinet, Thiết kế Website, Thiết kế Mobile App">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- all css here -->
+    <!-- bootstrap v3.3.6 css -->
+    <link rel="stylesheet" href="landing/serminar/css/bootstrap.min.css">
+    <!-- font-awesome css -->
+    <link rel="stylesheet" href="website/css/font-awesome.min.css">
+    <!-- landing/serminar/style.css -->
+    <link rel="stylesheet" href="landing/serminar/style.css">
+    <!-- style.css -->
+    <link rel="stylesheet" href="landing/vn-web-app-design/css/style.css">
+</head>
+
+<body data-spy="scroll">
+    <div id="preloader"></div>
+    <header>
+        <!-- header-area start -->
+        <div id="sticker" class="header-area default-header hidden-xs">
+            <div class="container">
+                <div class="center">
+                    <!-- logo start -->
+                    <div class="col-md-3 col-sm-3"  style="margin-left: 50%;">
+                        <div class="logo">
+                            <!-- Brand -->
+                            <a class="navbar-brand page-scroll" href="https://hachinet.com/">
+                                <img src="landing/serminar/img/logo/hachinet.png" alt="hachinet logo">
+                            </a>
+                        </div>
+                    </div>
+                    <!-- logo end -->
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- header-area end -->
+    </header>
+    <!-- header end -->
+    <!-- Start Slider Area -->
+    <div id="home" class="slide-area parallax-bg fix" data-stellar-background-ratio="0.5">
+        <div class="display-table">
+            <div class="display-table-cell">
+                <div class="container">
+                    <div class="row">
+                        <!-- Start Slider content -->
+                        <div class="intro-content text-center">
+                            <div class="slide-content">
+                                <h1 class="title1">Thiết kế</h1>
+                                <h2 class="title2" style="text-transform: none">Website</h2>
+                                <h2 class="title2" style="text-transform: none">Mobile App</h2>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End Slider content -->
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Start Service Area -->
+    <div class="area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-head text-center">
+                        <h3>Cung cấp <span class="color">Dịch vụ</span></h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-head text-center">
+                        <h3>Sản phẩm <span class="color">Đã làm</span></h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-head text-center">
+                        <h3>Khách hàng <span class="color">nói gì về Hachinet</span></h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <div class="area-padding">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-head text-center">
+                        <h3>Đối tác <span class="color">của chúng tôi</span></h3>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+    </div>
+    <!-- End Service Area -->
+
+    <!-- Start contact Area -->
+    <div id="contact" class="contact-page area-padding img-contact">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 col-sm-12 col-xs-12">
+                    <div class="section-head text-center">
+                        <h3>Liên hệ với <span class="color">chúng tôi</span></h3>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="contact-head">
+                        <div class="contact-icon">
+                            <div class="single-contact">
+                                <br /><br />
+                                <h5>Địa chỉ:</h5>
+                                <a><i class="fa fa-map"></i><span>2F - 21B6, 234 Pham Van Dong, Hanoi,
+                                    Vietnam</span></a>
+                                <a><i class="fa fa-phone"></i><span>(+84) 24-6290-0388</span></a>
+                                <a><i class="fa fa-envelope"></i><span>contact@hachinet.com</span></a>
+                                <br />
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Start contact Form -->
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="contact-form">
+                        <div class="row">
+                            <form id="contactForm" method="POST" class="contact-form">
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="text" id="nameSuccess" class="form-control" placeholder="Name" required
+                                        data-error="Please enter your name">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="col-md-6 col-sm-6 col-xs-12">
+                                    <input type="email" class="email form-control" id="emailSuccess" placeholder="Email"
+                                        required data-error="Please enter your email">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <input type="text" id="msg_subject" class="form-control" placeholder="Subject"
+                                        required data-error="Please enter your message subject">
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="col-md-12 col-sm-12 col-xs-12">
+                                    <textarea id="message" rows="7" placeholder="Messages" class="form-control" required
+                                        data-error="Write your message"></textarea>
+                                    <div class="help-block with-errors"></div>
+                                </div>
+                                <div class="col-md-12 col-sm-12 col-xs-12 text-center">
+                                    <button type="button" id="submit" 
+                                    data-loading-text="<i class='fa fa-spinner fa-spin '></i> Đang gửi"    
+                                    class="contact-btn1" onclick="sendSuccess()">Gửi</button>
+                                    <div id="msgSubmit" class="h3 text-center hidden"></div>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                <!-- End contact Form -->
+            </div>
+        </div>
+    </div>
+
+    <!-- Start Footer Area -->
+    <footer>
+        <div class="footer-top-area area-padding img-footer">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12 col-sm-12 col-xs-12">
+                        <div class="footer-content text-center">
+                            <div class="single-footer">
+                                <div class="footer-logo text-center">
+                                    <a>
+                                        <img src="landing/serminar/img/logo/footer-logo.png" style="width: 20%; margin-left: 3px;" alt="">
+                                    </a>
+                                </div>
+                            </div>
+                            <div class="footer-social">
+                                <ul class="social-link">
+                                    <li><a href="https://www.facebook.com/hachinet.jsc/" target="_blank"><i
+                                                class="fa fa-facebook"></i></a></li>
+                                    <li><a href="https://www.linkedin.com/company/hachinet-software1/"
+                                            target="_blank"><i class="fa fa-linkedin"></i></a></li>
+                                    <li><a href="https://www.instagram.com/hachinet.software/" target="_blank"><i
+                                                class="fa fa-instagram"></i></a></li>
+                                    <li><a href="https://www.facebook.com/hachinet.jsc/" target="_blank"><i
+                                                class="fa fa-youtube-play"></i></a></li>
+                                </ul>
+                            </div>
+                            <div class="copyright">
+                                <p>
+                                    All rights reserved HACHINET Copyright © 2018 - 2020
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- End Footer Area -->
+
+    <!-- all js here -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script type="text/javascript">
+        function sendEmail() {
+            var name = $("#name");
+            var email = $("#email");
+            var address = $("#address");
+            var company = $("#company");
+
+            if (isNotEmpty(name) && isNotEmpty(email) && isNotEmpty(address) && isNotEmpty(company)) {
+                $('.slide-btn').button('loading');
+                $.ajax({
+                    url: 'online-seminar',
+                    method: 'POST',
+                    dataType: 'json',
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        name: name.val(),
+                        email: email.val(),
+                        address: address.val(),
+                        company: company.val()
+                    }, success: function (response) {
+                        swal("", "Cảm ơn bạn đã đăng ký, chúng tôi sẽ gửi cho bạn link URL buổi seminar qua email bạn đã đăng ký vào buổi tối trước ngày seminar được diễn ra (07/09/2020)", "success", { button: false });
+                        $('#myForm')[0].reset();
+                        $('.modal').hide();
+                        $('.slide-btn').button('reset');
+                        // alert('Cảm ơn bạn đã đăng ký, chúng tôi sẽ gửi cho bạn link URL buổi seminar qua email bạn đã đăng ký vào buổi tối trước ngày seminar được diễn ra (19/08/2020)')
+
+                    }
+                });
+            }
+        }
+
+        function sendSuccess() {
+            var nameSuccess = $("#nameSuccess");
+            var emailSuccess = $("#emailSuccess");
+            var msg_subject = $("#msg_subject");
+            var message = $("#message");
+            if (isNotEmpty(nameSuccess) && isNotEmpty(emailSuccess) && isNotEmpty(msg_subject) && isNotEmpty(message)) {
+                $('.contact-btn1').button('loading');
+                $.ajax({
+                    url: 'online-seminar-success',
+                    method: 'POST',
+                    dataType: 'json',
+                    data: {
+                        "_token": "{{ csrf_token() }}",
+                        nameSuccess: nameSuccess.val(),
+                        emailSuccess: emailSuccess.val(),
+                        msg_subject: msg_subject.val(),
+                        message: message.val()
+                    }, success: function (response) {
+                        swal("", "Thư của bạn đã được gửi thành công, cảm ơn bạn đã liên hệ với chúng tôi", "success");
+                        $('#contactForm')[0].reset();
+                        $('.contact-btn1').button('reset');
+                        // $('.modal').modal('hide');
+                    }
+                });
+            }
+        }
+
+        function isNotEmpty(caller) {
+            if (caller.val() == "") {
+                caller.css('border', '1px solid red');
+                return false;
+            } else
+                caller.css('border', '');
+
+            return true;
+        }
+
+    </script>
+    <script src="landing/serminar/js/vendor/app.js"></script>
+    <!-- jquery latest version -->
+    <script src="landing/serminar/js/vendor/jquery-1.12.4.min.js"></script>
+    <!-- bootstrap js -->
+    <script src="landing/serminar/js/bootstrap.min.js"></script>
+    <!-- owl-carousel js -->
+    <script src="landing/serminar/js/owl.carousel.min.js"></script>
+    <!-- Counter js -->
+    <script src="landing/serminar/js/jquery.counterup.min.js"></script>
+    <!-- waypoint js -->
+    <script src="landing/serminar/js/waypoints.js"></script>
+    <!-- isotope js -->
+    <script src="landing/serminar/js/isotope.pkgd.min.js"></script>
+    <!-- Meanmenu js -->
+    <script src="landing/serminar/js/jquery.meanmenu.js"></script>
+    <!-- Typed js -->
+    <script src="landing/serminar/js/typed.js"></script>
+    <!-- Easing js -->
+    <script src="landing/serminar/js/easing.js"></script>
+    <!-- Magnific js -->
+    <script src="landing/serminar/js/magnific.min.js"></script>
+    <!-- stellar js -->
+    <script src="landing/serminar/js/jquery.stellar.min.js"></script>
+    <!-- venobox js -->
+    <script src="landing/serminar/js/venobox.min.js"></script>
+    <!-- plugins js -->
+    <script src="landing/serminar/js/plugins.js"></script>
+    <!-- main js -->
+    <script src="landing/serminar/js/main.js"></script>
+</body>
+
+</html>
