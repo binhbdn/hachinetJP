75b63b6c8b55a633112f50a6bf8e4d9051eed3bc
binhbdn <binh.buidn@gmail.com>
2020-11-12 15:52:27 +0700

    New landing page: TEST Facebook SDK / Chat Plugin

--- resources/views/Website/landing/vn-web-mobile-app-design.blade.php
+++ resources/views/Website/landing/vn-web-mobile-app-design.blade.php
@@ -21,7 +21,33 @@
     <link rel="stylesheet" href="admin/css/animate.css">
 </head>
 
-<body class = "translator-vn" data-spy="scroll"></body>
+<body class = "translator-vn" data-spy="scroll">
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>
+        window.fbAsyncInit = function() {
+        FB.init({
+            xfbml            : true,
+            version          : 'v9.0'
+        });
+        };
+
+        (function(d, s, id) {
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) return;
+        js = d.createElement(s); js.id = id;
+        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+    </script>
+    <!-- Your Chat Plugin code -->
+    <div class="fb-customerchat"
+        attribution=setup_tool
+        page_id="112351537350806"
+        theme_color="#FFB701"
+        logged_in_greeting="Xin chào! HACHINET có thể giúp gì bạn?"
+        logged_out_greeting="Xin chào! HACHINET có thể giúp gì bạn?">
+    </div>
     <div id="preloader"></div>
     <!-- header-area start -->
     <header>
